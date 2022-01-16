b6409ee48c897e412e636a76ea6764dc063dd335
Mitchell R <support@bettercorp.co.za>
2022-01-16 16:47:16 +0200

    Bugfix for ip stuff...

--- src/plugins/fastify/ipHandlerPlugin.ts
+++ src/plugins/fastify/ipHandlerPlugin.ts
@@ -1,14 +1,14 @@
 import { IDictionary } from '@bettercorp/tools/lib/Interfaces';
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest } from 'fastify';
 import * as fp from 'fastify-plugin';
 
 function fastifyIPPlugin(instance: FastifyInstance, options: any, nextPlugin: { (): void; }) {
-  instance.addHook('onRequest', (req: any, res: any, next: { (): void; }) => {
+  instance.addHook('onRequest', (req: FastifyRequest, res: any, next: { (): void; }) => {
     let headerKeys: IDictionary<string> = {};
     for (let hKey of Object.keys(req.headers))
       headerKeys[hKey.toLowerCase()] = hKey;
 
-    req.ip = (req.headers[headerKeys['true-client-ip']] ||
+    req.headers['ip'] = (req.headers[headerKeys['true-client-ip']] ||
       req.headers[headerKeys['cf-connecting-ip']] ||
       req.headers[headerKeys['x-client-ip']] ||
       req.headers[headerKeys['x-forwarded-for']] ||
